da6fdfaef20fa0ce73ef050fe92b08968af5a340
Tim Watson <tswatson123@gmail.com>
2015-09-01 08:53:54 +0100

    Read configs from dir

--- src/clj/logging_dashboard/configs.clj
+++ src/clj/logging_dashboard/configs.clj
@@ -3,3 +3,8 @@
 (defn save 
   [config]
   (spit (str "dashboards/" (get-in config [:table-settings :name]) ".json") config))
+
+(defn get-all []
+  (let [dir (clojure.java.io/file "dashboards")
+        files (file-seq dir)]
+    (map slurp files)))
